32b0dfae8b9d04d91f3b0477c47f2594c3c0583d
GauravPoosarla <poosarlagaurav@gmail.com>
2023-01-14 19:54:38 +0530

    feat: add solutions of question 2

---
+++ question2.js
@@ -0,0 +1,39 @@
+// Regular function
+function checkAllDigitsSame(num) {
+    let lastDigit = num%10;
+    num /= 10;
+    num = Math.trunc(num);
+
+    while(num > 0)
+    {
+        let digit = num%10;
+        // console.log(lastDigit, digit);
+        if(digit !== lastDigit)
+            return false;
+        num /= 10;
+        num = Math.trunc(num);
+    }
+    return true;
+}
+
+// console.log(checkAllDigitsSame(23));
+
+// Arrow function
+const checkAllDigitsSameArrow = (num) => {
+    let lastDigit = num%10;
+    num /= 10;
+    num = Math.trunc(num);
+
+    while(num > 0)
+    {
+        let digit = num%10;
+        // console.log(lastDigit, digit);
+        if(digit !== lastDigit)
+            return false;
+        num /= 10;
+        num = Math.trunc(num);
+    }
+    return true;
+}
+
+// console.log(checkAllDigitsSameArrow(23));
