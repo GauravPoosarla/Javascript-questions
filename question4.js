f457b8c846a57ee490f040547a1b7196ab4730ca
GauravPoosarla <poosarlagaurav@gmail.com>
2023-01-14 20:26:42 +0530

    feat: add solutions of question 4

---
+++ question4.js
@@ -0,0 +1,29 @@
+// Regular Function
+
+function findLongestString(arr) {
+    let finalString = arr[0];
+    for(let idx=1; idx<arr.length; idx++)
+    {
+        if(arr[idx].length > finalString.length)
+        {
+            finalString = arr[idx];
+        }
+    }
+    return finalString;
+}
+
+// console.log(findLongestString(["we", "love", "code", "academy"]));
+
+// Arrow function using forEach and ternary operations.
+let findLongestStringArrow = (arr) => {
+    let finalString = arr[0];
+    arr.forEach(findLongestStringUtil);
+
+    function findLongestStringUtil(item) {
+        let x = item.length > finalString.length ? item : finalString;
+        finalString = x;
+    }
+    return finalString;
+}
+
+console.log(findLongestStringArrow(["we", "love", "code", "academy"]));
