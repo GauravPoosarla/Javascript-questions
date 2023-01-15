ab4ccd81371eacaa67b249c6b2aae5646486e709
GauravPoosarla <poosarlagaurav@gmail.com>
2023-01-15 16:13:54 +0530

    feat: add tests to replace every char with next char

---
+++ replaceEveryCharWithNextCharQ9/replaceEveryCharWithNextChar.js
@@ -0,0 +1,50 @@
+// Regular function
+
+function replaceEveryCharacterWithSuccessor(input) {
+    let finalString = "";
+    for(let idx=0; idx<input.length; idx++)
+    {
+        let charCode = input.charCodeAt(idx) + 1;
+        if(charCode == 123)
+        {
+            finalString += 'a';
+            continue;
+        }
+        if(charCode == 91)
+        {
+            finalString += 'A';
+            continue;
+        }
+        let char = String.fromCharCode(charCode);
+        finalString += char;
+    }
+    return finalString;
+}
+
+// console.log(replaceEveryCharacterWithSuccessor("codeacademy"));
+
+// Arrow function
+
+let replaceEveryCharacterWithSuccessorUsingArrowFunction = (input) => {
+    let finalString = "";
+    for(let idx=0; idx<input.length; idx++)
+    {
+        let charCode = input.charCodeAt(idx) + 1;
+        if(charCode == 123)
+        {
+            finalString += 'a';
+            continue;
+        }
+        if(charCode == 91)
+        {
+            finalString += 'A';
+            continue;
+        }
+        let char = String.fromCharCode(charCode);
+        finalString += char;
+    }
+    return finalString;
+}
+
+// console.log(replaceEveryCharacterWithSuccessorArrow("codeacademy"));
+module.exports = {replaceEveryCharacterWithSuccessor, replaceEveryCharacterWithSuccessorUsingArrowFunction}
---
+++ replaceEveryCharWithNextCharQ9/replaceEveryCharWithNextChar.test.js
@@ -0,0 +1,32 @@
+const { replaceEveryCharacterWithSuccessor } = require('./replaceEveryCharWithNextChar')
+const { replaceEveryCharacterWithSuccessorUsingArrowFunction } = require('./replaceEveryCharWithNextChar');
+
+describe('replace every char with the next alphabet', () => {
+    test('"codeacademy" should result in ""dpefbdbefnz""', () => {
+        expect(replaceEveryCharacterWithSuccessor("codeacademy")).toBe("dpefbdbefnz");
+    });
+    test('"lazyinterns" should result in "mbazjoufsot"', () => {
+        expect(replaceEveryCharacterWithSuccessor("lazyinterns")).toBe("mbazjoufsot");
+    });
+    test('"mckinsey" should result in "ndljoqfz"', () => {
+        expect(replaceEveryCharacterWithSuccessor("mckinsey")).toBe("ndljotfz");
+    });
+    test('"sdfghjk" should result in "teghikl"', () => {
+        expect(replaceEveryCharacterWithSuccessor("sdfghjk")).toBe("teghikl");
+    });
+});
+
+describe('replace every char with the next alphabet', () => {
+    test('"codeacademy" should result in ""dpefbdbefnz""', () => {
+        expect(replaceEveryCharacterWithSuccessorUsingArrowFunction("codeacademy")).toBe("dpefbdbefnz");
+    });
+    test('"lazyinterns" should result in "mbazjoufsot"', () => {
+        expect(replaceEveryCharacterWithSuccessorUsingArrowFunction("lazyinterns")).toBe("mbazjoufsot");
+    });
+    test('"mckinsey" should result in "ndljotfz"', () => {
+        expect(replaceEveryCharacterWithSuccessorUsingArrowFunction("mckinsey")).toBe("ndljotfz");
+    });
+    test('"sdfghjk" should result in "teghikl"', () => {
+        expect(replaceEveryCharacterWithSuccessorUsingArrowFunction("sdfghjk")).toBe("teghikl");
+    });
+});
